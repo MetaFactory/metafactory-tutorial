4f07a4327c366249ebaf5d5610e21ac95fc3ad9f
Merijn van Rossum <merijnvanrossum@gmail.com>
2016-12-06 17:17:23 +0100

    - format shell script removed as they are no longer necessary with the newest MetaFactory

--- project/format.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-rem Define the environment variables ECLIPSE_HOME and JAVA_HOME
-rem Change commands if needed
-rem The format.cmd is automatically called by win32 version of MetaFactory
-
-rem %ECLIPSE_HOME%\eclipse\eclipse.exe -nosplash -vm %JAVA_HOME%\bin\javaw.exe -application org.eclipse.jdt.core.JavaCodeFormatter -config org.eclipse.jdt.core.prefs %*
-%ECLIPSE_HOME%\jbdevstudio.exe -nosplash -vm %JAVA_HOME%\bin\javaw.exe -application org.eclipse.jdt.core.JavaCodeFormatter -config org.eclipse.jdt.core.prefs %*
--- project/format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_72.jdk/Contents/Home
-#ECLIPSE_HOME=/Applications/jbdevstudio-8.0.0-GA/studio/JBoss Developer Studio-8.1.0-GA.app/Contents/MacOS
-ECLIPSE_HOME=/Users/Shared/java/Eclipse-RCP-4.4/eclipse
-${ECLIPSE_HOME}/eclipse -nosplash -vm ${JAVA_HOME}/bin/java -application org.eclipse.jdt.core.JavaCodeFormatter -config `pwd`/org.eclipse.jdt.core.prefs $*
