31cb6f0d2f259f9c6b07ca22821419ff6162f698
marnix <vanbochove@firstbase.nl>
2019-01-02 11:24:52 +0100

    Remove generated folder

--- generated/src/main/java/org/metafactory/tutorial/example2/model/implementation/RelationType.java
+++
@@ -1,9 +0,0 @@
-package org.metafactory.tutorial.example2.model.implementation;
-
-/**
- * RelationType - Created by MetaFactory: Automation of Software Development
- */
-public enum RelationType {
-  Private, Business;
-
-}
